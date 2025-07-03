215f54b7ffb4dbfdf96eca73ae66ae4219b571b6
cphpi <chanpakho0325@yahoo.com.hk>
2025-07-03 18:53:21 +0100

    update npc cow

--- scenes/characters/npc_states/walk_state.gd
+++ scenes/characters/npc_states/walk_state.gd
@@ -21,6 +21,7 @@ func set_movment_target() -> void:
 	var target_position: Vector2 = NavigationServer2D.map_get_random_point(navigation_agent_2d.get_navigation_map(), navigation_agent_2d.navigation_layers, false)
 	navigation_agent_2d.target_position = target_position
 	speed = randf_range(min_speed, max_speed)	
+	
 func _on_process(_delta : float) -> void:
 	pass
 

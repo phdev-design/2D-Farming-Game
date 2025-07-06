ac22e665c2c0b9c59554eade7c53b5d2d2f451e3
cphpi <chanpakho0325@yahoo.com.hk>
2025-07-06 21:50:35 +0100

    update menu 2

--- scenes/ui/tools_panel.gd
+++ scenes/ui/tools_panel.gd
@@ -1,5 +1,6 @@
 extends PanelContainer
 
+@onready var tool_axe: Button = $MarginContainer/HBoxContainer/ToolAxe
 @onready var tool_tilling: Button = $MarginContainer/HBoxContainer/ToolTilling
 @onready var tool_watering_can: Button = $MarginContainer/HBoxContainer/ToolWateringCan
 @onready var tool_corn: Button = $MarginContainer/HBoxContainer/ToolCorn
@@ -9,6 +10,23 @@ extends PanelContainer
 func _on_tool_axe_pressed() -> void:
 	ToolManage.select_tool(DataTypes.Tools.AxeWood)
 
-
 func _on_tool_tilling_pressed() -> void:
-	pass # Replace with function body.
+	ToolManage.select_tool(DataTypes.Tools.TillGround)
+	
+func _on_tool_watering_can_pressed() -> void:
+	ToolManage.select_tool(DataTypes.Tools.WaterCrops)
+	
+func _on_tool_corn_pressed() -> void:
+	ToolManage.select_tool(DataTypes.Tools.PlantCorn)
+
+func _on_tool_tomato_pressed() -> void:
+	ToolManage.select_tool(DataTypes.Tools.PlantTomato)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("release_tool"):
+		ToolManage.select_tool(DataTypes.Tools.None)
+		tool_axe.release_focus()
+		tool_tilling.release_focus()
+		tool_watering_can.release_focus()
+		tool_corn.release_focus()
+		tool_tomato.release_focus()

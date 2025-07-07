b61f2fb4fd34045f68eb424cae788a0ee48e3856
cphpi <chanpakho0325@yahoo.com.hk>
2025-07-07 16:38:59 +0100

    update inventory

--- scenes/components/interactable_component.gd
+++ scenes/components/interactable_component.gd
@@ -7,6 +7,5 @@ signal interactable_deactivated
 func _on_body_entered(body: Node2D) -> void:
 	interactable_activated.emit()
 
-
 func _on_body_exited(body: Node2D) -> void:
 	interactable_deactivated.emit()

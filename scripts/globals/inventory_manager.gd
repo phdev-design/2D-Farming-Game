017234a6af09f9bb95a491a98c97dfc9385b0ade
cphpi <chanpakho0325@yahoo.com.hk>
2025-07-13 21:43:54 +0100

    update menu

--- scripts/globals/inventory_manager.gd
+++ scripts/globals/inventory_manager.gd
@@ -13,3 +13,14 @@ func add_collectable(collectable_name: String) -> void:
 		inventory[collectable_name] += 1
 		
 	inventory_changed.emit()
+	
+func remove_collectable(collectable_name: String) -> void:
+	inventory.get_or_add(collectable_name)
+	
+	if inventory[collectable_name] == null:
+		inventory[collectable_name] = 0
+	else:
+		if inventory[collectable_name] > 0:
+			inventory[collectable_name] -= 1
+		
+	inventory_changed.emit()
